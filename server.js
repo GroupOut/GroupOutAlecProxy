74acb06018a6c8728afc20c0e45cfe60f35c60aa
Alec Hyde <alechyde91@gmail.com>
2018-08-01 12:17:54 -0700

    adding updated

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(morgan('dev'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use('/deals/:id', express.static(path.join(__dirname, './public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
